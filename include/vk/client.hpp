222ad2a54a811e824528f7ec08c599eee990e049
Nikita Datsuk <ILLLIGION@users.noreply.github.com>
2016-11-09 14:11:09 +0300

    Create client.hpp

---
+++ include/vk/client.hpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <map>
+
+namespace vk{
+  class Client{
+  public:
+    using dict_t = std::map<std::string, std::string>;
+    Client(dict_t settings);
+    auto check_connection() -> bool;
+  };
+}
