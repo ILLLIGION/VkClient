209ccd4f9585cc02323b0afe619ce0b319b86cad
Nikita <dazuk07@yandex.ru>
2016-11-11 10:10:33 +0300

    build deleted

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/illligion/VkClient
-# Build directory: /home/illligion/VkClient/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(check "/home/illligion/VkClient/build/check" "-s" "-r" "compact" "--use-colour" "yes")
