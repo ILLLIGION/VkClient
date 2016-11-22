89b879afae8af2a9e35f643a325cc1c81b6ddb7d
Nikita Datsuk <ILLLIGION@users.noreply.github.com>
2016-11-23 00:11:24 +0300

    Update client.cpp

--- sources/client.cpp
+++ sources/client.cpp
@@ -91,7 +91,7 @@ namespace Vk
 	return friend_list;
     }
 
-    auto Client::write_callback(char* data, size_t size, size_t nmemb, void* buff) -> size_t
+    static auto Client::write_callback(char* data, size_t size, size_t nmemb, void* buff) -> size_t
     {
         int result = 0;
 
